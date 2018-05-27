8108454dfe3c322e437649c1a1dad121d170235e
nutsuda <nutsuda.aingly@gmail.com>
2018-05-27 17:08:48 +0700

    all code

---
+++ Aing/homepage.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Home Page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+    <style>
+        * {
+            font-family: 'Waffle Regular';
+        }
+
+        html {
+            background: url(home1.jpg) no-repeat center center fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+            font-family: 'Waffle Regular';
+            width: 100%;
+        }
+
+        body {
+            /* height: 100%; */
+            background-color: transparent;
+        }
+
+        box {
+            top: 350px;
+            border-radius: 15%
+        }
+
+        .button {
+            background-color: #76FF00;
+            background-position: center;
+            border: none;
+            padding: 0px 0px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 30px;
+            font-family: 'Waffle Regular';
+            border-radius: 12px;
+            color: white;
+        }
+        .button1 {
+            background-color: rgba(255, 255, 255,0.3);
+            background-position: center;
+            border: none;
+            padding: 0px 0px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 32px;
+            font-family: 'Waffle Regular';
+            border-radius: 12px;
+            color: white;
+        }
+
+    </style>
+</head>
+
+<body>
+    <div class="container-fluid" style="margin-top:305px;">
+        <div class="row">
+            <div class="col">
+                <div class="col-sm-4" style="color:white; font-size:35px; text-align:center; ">
+                    <div class="form-group">
+                        <a href="#" class="button1" style="width:30%">
+                            <img src="../webpage/house.png" alt="" width="30%"><strong> Home</strong>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-4" style="color:white; font-size:35px;text-align:center;">
+                    <div class="form-group">
+                        <a href="#" class="button1" style="width:30%">
+                            <img src="../pictures/login (1).png" alt="" width="30%"><strong> Log in</strong>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-4" style="color:white; font-size:35px;text-align:center;">
+                    <div class="form-group">
+                        <a href="#" class="button1" style="width:30%">
+                            <img src="../pictures/phone.png" alt="" width="30%"> <strong> Contact</strong>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-6">
+                <div class="col-sm-4" style="color:white; font-size:35px; text-align:center; ">
+                    <strong>Courses offered</strong>
+                    <div>
+                        <img src="../webpage/scholarship.png" alt="" width="30%">
+                    </div>
+                    <div class="form-group">
+                        <a href="#" class="button" style="width:30%">
+                            <img src="../pictures/123376.png" alt="" width="20%"> view more
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-4" style="color:white; font-size:35px;text-align:center;">
+                    <strong>Branch Information</strong>
+                    <div>
+                        <img src="../pictures/school.png" alt="" width="30%">
+                    </div>
+                    <div class="form-group">
+                        <a href="#" class="button" style="width:30%">
+                            <img src="../pictures/123376.png" alt="" width="20%"> view more
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-sm-4" style="color:white; font-size:35px;text-align:center;">
+                    <strong>Teacher Information</strong>
+                    <div>
+                        <img src="../pictures/presentation.png" alt="" width="30%">
+                    </div>
+                    <div class="form-group">
+                        <a href="#" class="button" style="width:30%">
+                            <img src="../pictures/123376.png" alt="" width="20%"> view more
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
---
+++ Aing/studentview.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Staff Information</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+
+
+    <style>
+        * {
+            font-family: 'Waffle Regular';
+            
+
+        }
+
+        .menubar {
+            background-color: #225A69;
+            /* padding: 15px; */
+            opacity: 0.85;
+            width: 100%;
+            color: white;
+            font-size: 30px;
+            text-align: right;
+
+        }
+
+        .element {
+            background-color: #119FA4;
+            /* padding: 15px; */
+            opacity: 0.85;
+            width: 100%;
+            color: white;
+            font-size: 50px;
+            text-align: left;
+            margin: 0px 0px;
+        }
+
+        .element2 {
+            background-color: rgba(17, 159, 164, 0.60);
+            /* padding: 15px; */
+            width: 100%;
+            color: white;
+            font-size: 30px;
+            text-align: center;
+            margin: 0px 0px;
+
+        }
+
+        .picstyle {
+            width: 50px;
+            position: absolute;
+            z-index: 999;
+            bottom: -160px;
+        }
+
+        .button1 {
+            background-color: rgba(255, 255, 255, 0.3);
+            background-position: center;
+            border: none;
+            padding: 0px 0px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 32px;
+            font-family: 'Waffle Regular';
+            border-radius: 12px;
+            color: white;
+
+        }
+
+h2{
+    font-size:50px;
+    color: navy;
+}
+        .form-group {
+            text-align: right;
+        }
+
+        .modal-body {
+            color: black;
+        }
+
+        .btn-primary {
+            font-size: 30px;
+            border-radius: 12px;
+        }
+
+        .table-bordered {
+            width: 100%;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="menubar">
+                <div class="col">
+                    <div class="col-sm-5" style="color:white; font-size:35px; text-align:center;margin-left:800px;">
+                        <div class="form-group">
+                            <a href="#" class="button1" style="width:250px">
+                                <img src="../webpage/house.png" alt="" width="30px">
+                                <strong> Home</strong>
+                            </a>
+                            <a href="#" class="button1" style="width:250px">
+                                <img src="../pictures/login (1).png" alt="" width="40px">
+                                <strong> Log out</strong>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-12" style="color:white; font-size:35px; text-align:left;">
+                        <div class="form-group">
+                            <a href="#" class="button1" style="width:250px">
+                                <strong> Hi, Username</strong>
+                            </a>
+                            <a href="#" class="button1" style="width:250px">
+                                <strong> Enrollment</strong>
+                            </a>
+                            <a href="#" class="button1" style="width:250px">
+                                <strong> Edit Profile</strong>
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid" style="background-color:rgba(185, 245, 252,0.6);">
+        <h2><strong>Enrollment Table</strong></h2>
+        <table class="table table-bordered" style="font-size:35px; text-align:center;">
+            <thead>
+                <tr>
+                    <th>Subject ID</th>
+                    <th>Subject Title</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>john@example.com</td>
+                </tr>
+                <tr>
+                    <td>Mary</td>
+                    <td>Moe</td>
+                    <td>mary@example.com</td>
+                </tr>
+                <tr>
+                    <td>July</td>
+                    <td>Dooley</td>
+                    <td>july@example.com</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+
+</body>
+
+</html>
