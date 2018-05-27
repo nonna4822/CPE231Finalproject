1ef0aab87c5cdbc10bf8472dc8bccd724b59fd97
Narawit <elesrubsoap_4822@hotmail.com>
2018-05-27 17:37:28 +0700

    update login_aing

--- login.php
+++ login.php
@@ -9,33 +9,57 @@ session_unset();
 // Set php variables
 $cardno = mysqli_real_escape_string($con,$_POST['cardno']);
 $tel =  mysqli_real_escape_string($con,$_POST['tel']);
+$choice = mysqli_real_escape_string($con,$_POST['choice']);
 
 // Check connection
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
-//query
-$sql = "SELECT firstname , lastname FROM student WHERE cardno = '$cardno' AND tel = '$tel'";
-$result = mysqli_query($con, $sql);
-$row = mysqli_fetch_assoc($result);
+//query student
+if($choice == 'student'){
 
-if($row['firstname'] == ""){
-  echo "เลขบัตรประชาชน หรือ เบอร์โทรศัพท์ ไม่ถูกต้อง !";
-  echo "<script>setTimeout(\"location.href = 'login.html';\",1500);</script>";
-}else {
-  $_SESSION['cardno'] = $cardno;
-  $_SESSION['tel'] = $tel;
-  $_SESSION['firstname'] = $row['firstname'];
-  $_SESSION['lastname'] = $row['lastname'];
-  if( substr( $cardno, 0, 5 ) === "admin"){
-    header("Location: adminview.php"); //go to student view
-    exit;
+    $sql = "SELECT firstname , lastname FROM student WHERE cardno = '$cardno' AND tel = '$tel'";
+    $result = mysqli_query($con, $sql);
+    $row = mysqli_fetch_assoc($result);
+
+    if($row['firstname'] == ""){
+      echo "เลขบัตรประชาชน หรือ เบอร์โทรศัพท์ ไม่ถูกต้อง !";
+      echo "<script>setTimeout(\"location.href = 'login.html';\",1500);</script>";
+    }else {
+      $_SESSION['cardno'] = $cardno;
+      $_SESSION['tel'] = $tel;
+      $_SESSION['firstname'] = $row['firstname'];
+      $_SESSION['lastname'] = $row['lastname'];
+
+      header("Location: studentview.php"); //go to student view
+      exit;
+    }
+
+  } else {
+  $sql = "SELECT firstname , lastname FROM staff WHERE cardno = '$cardno' AND tel = '$tel'";
+  $result = mysqli_query($con, $sql);
+  $row = mysqli_fetch_assoc($result);
+
+  if($row['firstname'] == ""){
+    echo "เลขบัตรประชาชน หรือ เบอร์โทรศัพท์ ไม่ถูกต้อง !";
+    echo "<script>setTimeout(\"location.href = 'login.html';\",1500);</script>";
   }else {
-    header("Location: studentview.php"); //go to student view
-    exit;
-  }
+    $_SESSION['cardno'] = $cardno;
+    $_SESSION['tel'] = $tel;
+    $_SESSION['firstname'] = $row['firstname'];
+    $_SESSION['lastname'] = $row['lastname'];
 
+    if( substr( $cardno, 0, 5 ) === "admin"){
+      header("Location: adminview.php"); //go to student view
+      exit;
+    }else {
+      header("Location: staffview.php"); //go to student view
+      exit;
+    }
+
+  }
 }
+
 
 //close sql
 mysqli_close($con);
