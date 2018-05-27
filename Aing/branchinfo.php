3c27245812ea47a70f82018ce2411b215c628397
nutsuda <nutsuda.aingly@gmail.com>
2018-05-28 03:07:14 +0700

    update

--- Aing/branchinfo.php
+++ Aing/branchinfo.php
@@ -3,6 +3,7 @@
 
 <head>
     <title>Branch Information</title>
+    <script src="myscripts.js"></script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
